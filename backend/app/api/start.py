339b126f47dc2587626721c18e90d1dd4efa15ed
UJwal <ujwal99arak@gmail.com>
2021-10-29 14:21:38 +0530

    trying websocket with fastapi

--- backend/app/api/start.py
+++ backend/app/api/start.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Body, Request, status
+from fastapi import APIRouter, Body, Request, status, WebSocket
+from fastapi import responses
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from .model import TaskModel
@@ -20,3 +21,22 @@ async def create_task(request: Request, task: TaskModel = Body(...)):
         {"_id": new_task.inserted_id}
     )
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_task)
+
+@api.post('/test/{id}', response_description="delete the task")
+async def remove_task(request: Request, id: str):
+    print(id)
+    deleted_task = await request.app.mongodb["tasks"].delete_one({'_id': id})
+    if deleted_task.deleted_count:
+        return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content={
+            'message': 'task has been deleted'
+        })
+    return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content={
+        'message': 'nothing to delete over here'
+    })
+
+@api.websocket('/ws')
+async def testing_with_sockets(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f'this is the text {data}')
